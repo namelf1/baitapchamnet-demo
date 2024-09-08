745e75cc76c622b30f6adec3b04bcc8925aee9dc
namelf <namelf@DESKTOP-EV1NID7>
2024-09-08 15:12:19 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace baitapchamnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//họ tên: Lương Thanh Nam
+//bắt đầu làm bài toán: chu vi hình vuông
