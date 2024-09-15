2e617be78e8a04b6f8b282871f8f849cce959d72
namelf <namelf@DESKTOP-EV1NID7>
2024-09-15 12:54:50 +0700

    add oop

--- Program.cs
+++ Program.cs
@@ -8,8 +8,32 @@ namespace baitapchamnet
     {
         static void Main(string[] args)
         {
+            //họ tên: Lương Thanh Nam
+            //bắt đầu làm bài toán: chu vi hình vuông
+            //input: chiều dài một cạnh hình vuông là số a thực
+            //output: chu vi p của hình vuông hoặc báo lỗi
+            //thuật toán:
+            // 1.kiểm tra được đầu vào a là số dương
+            // 2. tính chu vi p = a*4
+            // 3. output là p hoặc báo lỗi nếu a<=0
+
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Tính chu vi hình vuông");
+            Console.Write("Nhập chiều dài cạnh của hình vuông: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            if (a <= 0)
+            {
+                Console.WriteLine("Lỗi: Độ dài cạnh của hình vuông phải lớn hơn 0.");
+            }
+            else
+            {
+                // Tính chu vi
+                double p = 4 * a;
+                // Hiển thị chu vi
+                Console.WriteLine("Chu vi của hình vuông là: " + p);
+            }
+            Console.ReadKey();
         }
     }
 }
-//họ tên: Lương Thanh Nam
-//bắt đầu làm bài toán: chu vi hình vuông
